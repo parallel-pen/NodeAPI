0eca4938f9bd1d02d5f10da4c492b8d71dd82450
Raylin51 <652112498@qq.com>
2018-05-20 20:20:21 +0800

    bug fix

--- actions/node.js
+++ actions/node.js
@@ -11,12 +11,15 @@ exports.content = async (req, res) => {
   const user = await checkToken(token);
   const descriptor = {
     query: {
-      nodeId: {type: "string", required: false},
-      first: {type: "number", required: false}
+      type: "object", required: true,
+      fields: {
+        nodeId: {type: "string", required: false},
+        first: {type: "number", required: false}
+      }
     }
   }
   let validator = new schema(descriptor);
-  validator.validate({query: req.query}, (errors, fields) => {
+  validator.validate({query: req.query}, async (errors, fields) => {
     if(errors) {
       res.send({
         code: 200000,
@@ -24,82 +27,82 @@ exports.content = async (req, res) => {
       });
       return;
     }
-  });
-  if (!user) {
-    res.statusCode = 401;
-    res.send({
-      code: 300000,
-      msg: '登录信息过期'
-    });
-    return;
-  }
-  if (!first && !nodeId) {
-    res.send({
-        code: 200000,
-        msg: '参数为空'
-    });
-    return;
-  }
-  let uid = user[0]._id;
-  let nodeOpt = {
-    type: 'find',
-    table: 'nodes',
-    query: {
-      [nodeId ? '_id' : 'first']: nodeId || true
+    if (!user) {
+      res.statusCode = 401;
+      res.send({
+        code: 300000,
+        msg: '登录信息过期'
+      });
+      return;
     }
-  };
-  let nodeFind = await db(nodeOpt);
-  if (nodeFind.length === 0) {
-    res.send({
-      code: 200012,
-      msg: 'Id错误'
-    });
-    return;
-  }
-  let { _id, father_id, content, desc, child_nodes, author } = nodeFind[0];
-  let _str = _id.toString().substr(0, 8);
-  let timestamp = new Date(Number(parseInt(_str, 16).toString() + '000'));
-  const childOpt = {
-    type: 'find',
-    table: 'nodes',
-    query: {
-      father_id: _id
+    if (!first && !nodeId) {
+      res.send({
+          code: 200000,
+          msg: '参数为空'
+      });
+      return;
     }
-  };
-  let childNodes = await db(childOpt);
-  let childList = Array();
-  childNodes.map((item, index) => {
-    let node = {
-      nodeId: item._id,
-      desc: item.desc
+    let uid = user[0]._id;
+    let nodeOpt = {
+      type: 'find',
+      table: 'nodes',
+      query: {
+        [nodeId ? '_id' : 'first']: nodeId || true
+      }
     };
-    childList.push(node);
-  });
-  const viewOpt = {
-    type: 'updateOne',
-    table: 'users',
-    query: {
-      _id: uid
-    },
-    data: {
-      $set: {
-        recent_view: {
-          desc: desc,
-          nodeId: _id
+    let nodeFind = await db(nodeOpt);
+    if (nodeFind.length === 0) {
+      res.send({
+        code: 200012,
+        msg: 'Id错误'
+      });
+      return;
+    }
+    let { _id, father_id, content, desc, child_nodes, author } = nodeFind[0];
+    let _str = _id.toString().substr(0, 8);
+    let timestamp = new Date(Number(parseInt(_str, 16).toString() + '000'));
+    const childOpt = {
+      type: 'find',
+      table: 'nodes',
+      query: {
+        father_id: _id
+      }
+    };
+    let childNodes = await db(childOpt);
+    let childList = Array();
+    childNodes.map((item, index) => {
+      let node = {
+        nodeId: item._id,
+        desc: item.desc
+      };
+      childList.push(node);
+    });
+    const viewOpt = {
+      type: 'updateOne',
+      table: 'users',
+      query: {
+        _id: uid
+      },
+      data: {
+        $set: {
+          recent_view: {
+            desc: desc,
+            nodeId: _id
+          }
         }
       }
     }
-  }
-  db(viewOpt);
-  res.send({
-    code: 100000,
-    nodeId: _id,
-    fatherId: father_id,
-    content: content,
-    desc: desc,
-    timestamp: timestamp,
-    author: author,
-    childNodes: childList
+    db(viewOpt);
+    res.send({
+      code: 100000,
+      nodeId: _id,
+      fatherId: father_id,
+      content: content,
+      desc: desc,
+      timestamp: timestamp,
+      author: author,
+      childNodes: childList
+    });
   });
 };
 
@@ -145,13 +148,16 @@ exports.create = async (req, res) => {
   const user = await checkToken(token);
   const descriptor = {
     query: {
-      content: {type: "string", required: true},
-      desc: {type: "string", required: true},
-      fatherId: {type: "string", required: true}
+      type: "object", required: true,
+      fields: {
+        content: {type: "string", required: true},
+        desc: {type: "string", required: true},
+        fatherId: {type: "string", required: true}
+      }
     }
   }
   let validator = new schema(descriptor);
-  validator.validate({query: req.body}, (errors, fields) => {
+  validator.validate({query: req.body}, async (errors, fields) => {
     if(errors) {
       res.send({
         code: 200000,
@@ -159,68 +165,68 @@ exports.create = async (req, res) => {
       });
       return;
     }
-  });
-  if (!user) {
-    res.statusCode = 401;
+    if (!user) {
+      res.statusCode = 401;
+      res.send({
+        code: 300000,
+        msg: '登录信息过期'
+      });
+      return;
+    }
+    if (!content || !desc || !fatherId) {
+      res.send({
+        code: 200000,
+        msg: '参数为空'
+      });
+      return;
+    }
+    if (content.length > 999) {
+      res.send({
+        code: 200012,
+        msg: '内容字数不符合要求'
+      });
+      return;
+    }
+    if (desc.length > 50) {
+      res.send({
+        code: 200013,
+        msg: '描述字数不符合要求'
+      });
+    }
+    let hash = sha256(sha256(content));
+    let uid = user[0]._id;
+    let nodeOpt = {
+      type: 'insert',
+      table: 'nodes',
+      query: {
+        content: content,
+        desc: desc,
+        author_id: uid,
+        author: user[0].account,
+        hash: hash,
+        father_id: fatherId,
+        child_nodes: false
+      }
+    };
+    let nodeCreate = await db(nodeOpt);
+    if (nodeCreate.errmsg !== undefined) {
+      res.send({
+        code: 200011,
+        msg: '内容重复'
+      });
+      return;
+    }
+    let _str = nodeCreate.ops[0]._id.toString().substr(0, 8);
+    let timestamp = new Date(Number(parseInt(_str, 16).toString() + '000'));
     res.send({
-      code: 300000,
-      msg: '登录信息过期'
-    });
-    return;
-  }
-  if (!content || !desc || !fatherId) {
-    res.send({
-      code: 200000,
-      msg: '参数为空'
-    });
-    return;
-  }
-  if (content.length > 999) {
-    res.send({
-      code: 200012,
-      msg: '内容字数不符合要求'
-    });
-    return;
-  }
-  if (desc.length > 50) {
-    res.send({
-      code: 200013,
-      msg: '描述字数不符合要求'
-    });
-  }
-  let hash = sha256(sha256(content));
-  let uid = user[0]._id;
-  let nodeOpt = {
-    type: 'insert',
-    table: 'nodes',
-    query: {
+      code: 100000,
+      nodeId: nodeCreate.ops[0]._id,
+      fatherId: fatherId,
       content: content,
       desc: desc,
-      author_id: uid,
-      author: user[0].account,
-      hash: hash,
-      father_id: fatherId,
-      child_nodes: false
-    }
-  };
-  let nodeCreate = await db(nodeOpt);
-  if (nodeCreate.errmsg !== undefined) {
-    res.send({
-      code: 200011,
-      msg: '内容重复'
+      timestamp: timestamp,
+      authorId: uid,
+      childNodes: false
     });
-    return;
-  }
-  let _str = nodeCreate.ops[0]._id.toString().substr(0, 8);
-  let timestamp = new Date(Number(parseInt(_str, 16).toString() + '000'));
-  res.send({
-    code: 100000,
-    nodeId: nodeCreate.ops[0]._id,
-    fatherId: fatherId,
-    content: content,
-    desc: desc,
-    timestamp: timestamp,
-    authorId: uid,
-    childNodes: false
   });
 };
--- actions/user.js
+++ actions/user.js
@@ -10,186 +10,54 @@ exports.login = async (req, res) => {
   const { account, password } = req.body;
   const descriptor = {
     query: {
-      account: {type: "string", required: true},
-      password: {type: "string", required: true}
+      type: "object", required: true,
+      fields: {
+        account: {type: "string", required: true},
+        password: {type: "string", required: true}
+      }
     }
   }
   let validator = new schema(descriptor);
-  validator.validate({query: req.body}, (errors, fields) => {
+  validator.validate({query: req.body}, async (errors, fields) => {
     if(errors) {
       res.send({
         code: 200000,
         msg: '参数格式不正确'
       });
+      return;
     }
-    return;
-  });
-  if (!account || !password) {
-    res.send({
-      code: 200000,
-      msg: '参数为空'
-    });
-    return;
-  }
-  let opt = {
-    type: 'find',
-    table: 'users',
-    query: {
-      account: account
+    if (!account || !password) {
+      res.send({
+        code: 200000,
+        msg: '参数为空'
+      });
+      return;
     }
-  };
-  let usersData = await db(opt);
-  if (usersData.length === 0) {
-    res.send({
-      code: 200001,
-      msg: '用户不存在'
-    });
-    return;
-  }
-  let userInfo = usersData[0];
-  let hash = userInfo.password;
-  let result = await bcrypt.compare(password, hash).catch(err => {
-    res.send({
-      code: 200002,
-      msg: '密码错误'
-    });
-    return;
-  });
-  if (result) {
-    let time = new Date().getTime();
-    let token = sha256(sha256(toString(userInfo._id)) + time);
-    let tokenUpdate = {
-      type: 'updateOne',
+    let opt = {
+      type: 'find',
       table: 'users',
       query: {
         account: account
-      },
-      data: {
-        $set: {
-          token: token
-        }
       }
     };
-    db(tokenUpdate);
-    res.send({
-      code: 100000,
-      uid: userInfo._id,
-      token: token,
-      username: userInfo.username,
-      myContent: userInfo.content,
-      recentView: userInfo.recent_view
-    });
-    return;
-  } else {
-    res.send({
-      code: 200002,
-      msg: '密码错误'
-    });
-    return;
-  }
-};
-
-// 注册
-exports.register = async (req, res) => {
-  const { account, password, inviteCode } = req.body;
-  const descriptor = {
-    query: {
-      account: {type: "string", required: true},
-      password: {type: "string", required: true},
-      inviteCode: {type: "string", required: true}
-    }
-  }
-  let validator = new schema(descriptor);
-  validator.validate({query: req.body}, (errors, fields) => {
-    if(errors) {
+    let usersData = await db(opt);
+    if (usersData.length === 0) {
       res.send({
-        code: 200000,
-        msg: '参数格式不正确'
+        code: 200001,
+        msg: '用户不存在'
       });
+      return;
     }
-    return;
-  });
-  if (!account || !password || !inviteCode) {
-    res.send({
-      code: 200000,
-      msg: '参数为空'
+    let userInfo = usersData[0];
+    let hash = userInfo.password;
+    let result = await bcrypt.compare(password, hash).catch(err => {
+      res.send({
+        code: 200002,
+        msg: '密码错误'
+      });
+      return;
     });
-    return;
-  }
-  let codeOpt = {
-    type: 'find',
-    table: 'invitations',
-    query: {
-      code: inviteCode
-    }
-  };
-  if (escape(password).indexOf('%u') > 0) {
-    res.send({
-      code: 200004,
-      msg: '密码不能有非英文以及特殊符号字符噢'
-    });
-    return;
-  }
-  if (account.length > 20) {
-    res.send({
-      code: 200005,
-      msg: '账号长度错误'
-    });
-    return;
-  }
-  if (password.length > 18 || password.length < 6) {
-    res.send({
-      code: 200006,
-      msg: '密码长度错误'
-    });
-    return;
-  }
-  let checkCode = await db(codeOpt);
-  if (checkCode.length === 0) {
-    res.send({
-      code: 200007,
-      msg: '邀请码不存在'
-    });
-    return;
-  } else if (!checkCode[0].available) {
-    res.send({
-      code: 200008,
-      msg: '邀请码已被使用'
-    });
-    return;
-  }
-  bcrypt
-    .hash(password, saltRounds)
-    .then(async result => {
-      let opt = {
-        type: 'insertOne',
-        table: 'users',
-        query: {
-          account: account,
-          password: result,
-          content: [],
-          recent_view: {}
-        }
-      };
-      let usersData = await db(opt);
-      if (
-        usersData.errmsg !== undefined &&
-        usersData.errmsg.split('"')[1] === account
-      ) {
-        res.send({
-          code: 200009,
-          msg: '账号已经被用过啦'
-        });
-        return;
-      }
-      codeOpt.type = 'updateOne';
-      codeOpt.data = {
-        $set: {
-          available: false
-        }
-      };
-      let invate_code_use = await db(codeOpt);
-      let userInfo = usersData.ops[0];
+    if (result) {
       let time = new Date().getTime();
       let token = sha256(sha256(toString(userInfo._id)) + time);
       let tokenUpdate = {
@@ -209,17 +77,155 @@ exports.register = async (req, res) => {
         code: 100000,
         uid: userInfo._id,
         token: token,
-        account: account,
+        username: userInfo.username,
         myContent: userInfo.content,
         recentView: userInfo.recent_view
       });
-    })
-    .catch(error => {
+      return;
+    } else {
       res.send({
-        code: 400000,
-        msg: error
+        code: 200002,
+        msg: '密码错误'
       });
-    });
+      return;
+    }
+  });
+};
+
+// 注册
+exports.register = async (req, res) => {
+  const { account, password, inviteCode } = req.body;
+  const descriptor = {
+    query: {
+      type: "object", required: true,
+      fields: {
+        account: {type: "string", required: true},
+        password: {type: "string", required: true},
+        inviteCode: {type: "string", required: true}
+      }
+    }
+  }
+  let validator = new schema(descriptor);
+  validator.validate({query: req.body}, async (errors, fields) => {
+    if(errors) {
+      res.send({
+        code: 200000,
+        msg: '参数格式不正确'
+      });
+      return;
+    }
+    if (!account || !password || !inviteCode) {
+      res.send({
+        code: 200000,
+        msg: '参数为空'
+      });
+      return;
+    }
+    let codeOpt = {
+      type: 'find',
+      table: 'invitations',
+      query: {
+        code: inviteCode
+      }
+    };
+    if (escape(password).indexOf('%u') > 0) {
+      res.send({
+        code: 200004,
+        msg: '密码不能有非英文以及特殊符号字符噢'
+      });
+      return;
+    }
+    if (account.length > 20) {
+      res.send({
+        code: 200005,
+        msg: '账号长度错误'
+      });
+      return;
+    }
+    if (password.length > 18 || password.length < 6) {
+      res.send({
+        code: 200006,
+        msg: '密码长度错误'
+      });
+      return;
+    }
+    let checkCode = await db(codeOpt);
+    if (checkCode.length === 0) {
+      res.send({
+        code: 200007,
+        msg: '邀请码不存在'
+      });
+      return;
+    } else if (!checkCode[0].available) {
+      res.send({
+        code: 200008,
+        msg: '邀请码已被使用'
+      });
+      return;
+    }
+    bcrypt
+      .hash(password, saltRounds)
+      .then(async result => {
+        let opt = {
+          type: 'insertOne',
+          table: 'users',
+          query: {
+            account: account,
+            password: result,
+            content: [],
+            recent_view: {}
+          }
+        };
+        let usersData = await db(opt);
+        if (
+          usersData.errmsg !== undefined &&
+          usersData.errmsg.split('"')[1] === account
+        ) {
+          res.send({
+            code: 200009,
+            msg: '账号已经被用过啦'
+          });
+          return;
+        }
+        codeOpt.type = 'updateOne';
+        codeOpt.data = {
+          $set: {
+            available: false
+          }
+        };
+        let invate_code_use = await db(codeOpt);
+        let userInfo = usersData.ops[0];
+        let time = new Date().getTime();
+        let token = sha256(sha256(toString(userInfo._id)) + time);
+        let tokenUpdate = {
+          type: 'updateOne',
+          table: 'users',
+          query: {
+            account: account
+          },
+          data: {
+            $set: {
+              token: token
+            }
+          }
+        };
+        db(tokenUpdate);
+        res.send({
+          code: 100000,
+          uid: userInfo._id,
+          token: token,
+          account: account,
+          myContent: userInfo.content,
+          recentView: userInfo.recent_view
+        });
+      })
+      .catch(error => {
+        res.send({
+          code: 400000,
+          msg: error
+        });
+      });
+  });
 };
 
 // 用户数量
